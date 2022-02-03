2dd34be916b2bbbed35e0c2d264f6c80ae156fae
DougMont <douglas_monteiro94@outlook.com>
2022-02-03 16:04:54 -0300

    Atualizacao css background

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 let busca_btn = document.querySelector("#busca")
 
+//adicione um ouvinte de evento ao botão que executa a função paiNasa quando é clicado
 busca_btn.addEventListener("click", ()=>{
     console.log("button pressed")
     paiNasa()
 })
 
+//Uma função assíncrona para buscar dados da API.
 async function paiNasa(){
-    let chavinha = "8kLTRQ65abdOpgmvugfbH3pcOcD597ZhEiDlbjcp"
-    let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${chavinha}`);
+    let chavinha = "y2gwcC29ClbjBI8V83DmJ8hEO9SMB1QvdsQsu3NZ"
+    let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${chavinha}`)
     console.log(resposta)
     let data = await resposta.json()
     console.log(data)
     diaApi(data)
 }
 
+//função que faz algo com os dados recebidos da API. O nome da função deve ser personalizado para o que você está fazendo com os dados
 function diaApi(data){
     document.querySelector("#conteudo").innerHTML += data.explanation
     document.querySelector("#conteudo").innerHTML += `<img src="${data.url}">`
